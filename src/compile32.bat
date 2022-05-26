84b324bb93d5345535bb22aa282b6fbcf77aed9e
senator <senat0r@pm.me>
2022-05-26 18:27:24 +0100

    Re-organize entire project layout

--- src/compile32.bat
+++ src/compile32.bat
@@ -6,7 +6,7 @@ taskkill /f /im ida.exe
 :: create temp folder
 mkdir obj
 
-set OUTPUT_FILE=fusion.dll
+set OUTPUT_FILE=..\\fusion.dll
 set BUILD_FOR=32
 make make_objects -j%NUMBER_OF_PROCESSORS%
 make make_output
@@ -14,4 +14,4 @@ make make_output
 :: delete temp folder
 rmdir /S /Q obj
 
-copy /Y "%OUTPUT_FILE%" "C:\Program Files\IDA77\plugins\fusion.dll"
+copy /Y "%cd%\%OUTPUT_FILE%" "C:\Program Files\IDA77\plugins\fusion.dll"
