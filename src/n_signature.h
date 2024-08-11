280b7ec7c36d2612e988440801bb222ce3dd9b89
Patoke <patokekawaii@gmail.com>
2024-08-11 01:12:21 -0400

    ~ use user defined address if present for binary pattern

--- src/n_signature.h
+++ src/n_signature.h
@@ -41,10 +41,10 @@ namespace n_signature{
     ea_t ea_max = 0;
     n_utils::get_text_min_max(ea_min, ea_max);
 
-    compiled_binpat_vec_t sig_data{};
-    parse_binpat_str(&sig_data, ea_min, signature.c_str(), 16);
-
     ea_t addr = (find_settings.start_at_addr > 0 ? find_settings.start_at_addr : ea_min) - 1;
+
+    compiled_binpat_vec_t sig_data{};
+    parse_binpat_str(&sig_data, addr, signature.c_str(), 16);
     while(true){
       addr = bin_search3(addr + 1, ea_max, sig_data, BIN_SEARCH_NOCASE | BIN_SEARCH_FORWARD);
 
