286a82c793e3b370c6e4c32ee728923c6352d3b3
senator <github@cdma.one>
2024-10-11 22:51:22 +0100

    Added FNV1-A and CRC-32 signature generation

--- src/c_signature_generator.h
+++ src/c_signature_generator.h
@@ -2,7 +2,9 @@
 
 enum e_signature_style{
   SIGNATURE_STYLE_CODE = 0,
-  SIGNATURE_STYLE_IDA  = 1
+  SIGNATURE_STYLE_IDA  = 1,
+  SIGNATURE_STYLE_FNV1A = 2,
+  SIGNATURE_STYLE_CRC32 = 3
 };
 
 class c_signature_generator{
@@ -41,14 +43,18 @@ public:
   }
 
   u32 get_sig_len_per_byte(e_signature_style style){
-    if(style == SIGNATURE_STYLE_CODE)
-      return 6;
-
-    if(style == SIGNATURE_STYLE_IDA)
-      return 4;
-
-    error("[Fusion] get_sig_len_per_byte failed with %i\n", style);
-    return -1;
+    switch (style){
+      case SIGNATURE_STYLE_CODE:
+        return 6;
+      case SIGNATURE_STYLE_IDA:
+        return 4;
+      case SIGNATURE_STYLE_FNV1A:
+      case SIGNATURE_STYLE_CRC32:
+        return 8;
+      default:
+        error("[Fusion] get_sig_len_per_byte failed with %i\n", style);
+        return -1;
+    }
   }
 
   // Memory returned from this function must be free'd
@@ -65,37 +71,62 @@ public:
     i8* sig = (i8*)malloc(sig_len);
     memset(sig, 0, sig_len);
 
-    for(u32 i = 0; i < bytes.size(); i++){
-      if((strlen(sig) + sig_len_per_byte) > sig_len){
-        warning("[Fusion] `0x%llX` Has a bugged signature buffer (0)", sig);
-        break;
-      }
-
-      if(style == SIGNATURE_STYLE_IDA){
-        if(i > 0)
-          qsnprintf(sig + strlen(sig), sig_len_per_byte, " ");
-
-        qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? ((n_settings::data & FLAG_USE_DUAL_QUESTION_MARKS) ? "??" : "?") : "%02X", (u8)bytes[i]);
-      }
-      else if(style == SIGNATURE_STYLE_CODE)
-        qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? ((n_settings::data & FLAG_USE_UNICODE_WILDCARD) ? "\\x2A" : "\\x00") : "\\x%02X", (u8)bytes[i]);
-    }
-
-    // Add the code signature mask onto the signature
-    if(style == SIGNATURE_STYLE_CODE && (n_settings::data & FLAG_INCLUDE_MASK_FOR_CODE_SIGS)){
-      // Add a space between the signature and the mask
-      qsnprintf(sig + strlen(sig), sig_len_per_byte, " ");
-
+    if (style == SIGNATURE_STYLE_FNV1A){
+      qsnprintf(sig, sig_len, "0x%08X", generate_fnv1a_hash());
+    } else if (style == SIGNATURE_STYLE_CRC32){
+      qsnprintf(sig, sig_len, "0x%08X", generate_crc32_hash());
+    }else{
       for(u32 i = 0; i < bytes.size(); i++){
         if((strlen(sig) + sig_len_per_byte) > sig_len){
-          warning("[Fusion] `0x%llX` Has a bugged signature buffer (1)", sig);
+          warning("[Fusion] `0x%llX` Has a bugged signature buffer (0)", sig);
           break;
         }
 
-        qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? "?" : "x");
+        if(style == SIGNATURE_STYLE_IDA){
+          if(i > 0)
+            qsnprintf(sig + strlen(sig), sig_len_per_byte, " ");
+
+          qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? ((n_settings::data & FLAG_USE_DUAL_QUESTION_MARKS) ? "??" : "?") : "%02X", (u8)bytes[i]);
+        }
+        else if(style == SIGNATURE_STYLE_CODE)
+          qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? ((n_settings::data & FLAG_USE_UNICODE_WILDCARD) ? "\\x2A" : "\\x00") : "\\x%02X", (u8)bytes[i]);
+      }
+
+      // Add the code signature mask onto the signature
+      if(style == SIGNATURE_STYLE_CODE && (n_settings::data & FLAG_INCLUDE_MASK_FOR_CODE_SIGS)){
+        // Add a space between the signature and the mask
+        qsnprintf(sig + strlen(sig), sig_len_per_byte, " ");
+
+        for(u32 i = 0; i < bytes.size(); i++){
+          if((strlen(sig) + sig_len_per_byte) > sig_len){
+            warning("[Fusion] `0x%llX` Has a bugged signature buffer (1)", sig);
+            break;
+          }
+
+          qsnprintf(sig + strlen(sig), sig_len_per_byte, imm[i] ? "?" : "x");
+        }
       }
     }
 
     return sig;
+  }
+
+  u32 generate_fnv1a_hash() const{
+    u32 hash = 0x811c9dc5;
+    for (u8 byte : bytes)
+      hash = ((hash ^ byte) * 0x01000193) & 0xFFFFFFFF;
+
+    return hash;
+  }
+
+  u32 generate_crc32_hash() const{
+    u32 crc = 0xFFFFFFFF;
+    for (u8 byte : bytes){
+      crc ^= byte;
+      for(u32 i = 0; i < 8; i++)
+        crc = (crc >> 1) ^ (0xEDB88320 & (-(crc & 1)));
+    }
+
+    return ~crc;
   }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@ bool idaapi plugin_run(size_t arg){
     "IDA-Fusion for IDA %.1f+\n"
     "<#Generate signature (CODE Style):R>\n"
     "<#Generate signature (IDA Style):R>\n"
-    "<#Search for a signature:R>\n"
-    "<#Configure settings:R>>", (float)IDA_SDK_VERSION / 100.f
+    "<#Generate signature (CRC-32 Style):R>\n"
+    "<#Generate signature (FNV1-A Style):R>\n"
+    "<#Search for a signature (CODE/IDA):R>\n"
+    "<#Configure settings:R>>\n", (float)IDA_SDK_VERSION / 100.f
   ).c_str();
 
   static i32  choice  = 0;
@@ -29,9 +31,21 @@ bool idaapi plugin_run(size_t arg){
       break;
     }
     case 2:{
+      show_wait_box("[Fusion] Creating CRC-32 signature...");
+      n_signature::create(SIGNATURE_STYLE_CRC32);
+      hide_wait_box();
+      break;
+    }
+    case 3:{
+      show_wait_box("[Fusion] Creating FNV-1A signature...");
+      n_signature::create(SIGNATURE_STYLE_FNV1A);
+      hide_wait_box();
+      break;
+    }
+    case 4:{
       static i8 signature_to_find[1024];
       if(!ask_form(
-        "Fusion — Enter signature\n"
+        "Fusion — Enter CODE/IDA signature\n"
         "<Signature:A5:1024:100>"
       , &signature_to_find))
         break;
@@ -39,7 +53,7 @@ bool idaapi plugin_run(size_t arg){
       n_signature::find(signature_to_find, {false, (bool)(n_settings::data & FLAG_STOP_AT_FIRST_SIGNATURE_FOUND), 0, 0, static_cast<bool>(n_settings::data & FLAG_AUTO_JUMP_TO_FOUND_SIGNATURES)});
       break;
     }
-    case 3:{
+    case 5:{
       n_settings::show_settings_dialog();
       break;
     }
